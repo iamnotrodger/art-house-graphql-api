0f8c7f3b7e2971bde2e20bae88a4d1c0513d17bb
iamnotrodger <rodger.retanal24@gmail.com>
2022-01-15 07:56:47 -0500

    [feature] Connected to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const dotenv = require('dotenv');
 const { ApolloServer } = require('apollo-server');
+const { MongoClient } = require('mongodb');
 const typeDefs = require('./schema');
 
 dotenv.config();
 
+const client = new MongoClient(process.env.MONGO_URI);
+client.connect();
+
 const server = new ApolloServer({ typeDefs });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
 	console.log(`🚀  Server ready at ${url}`);
 });
