39878692937c10a1579dada838ae0d372a209fbc
iamnotrodger <rodger.retanal24@gmail.com>
2022-01-17 15:43:00 -0500

    [update] Artist Datasource `getManyByIDs` function

--- src/datasources/artist.js
+++ src/datasources/artist.js
@@ -3,14 +3,24 @@ const { MongoDataSource } = require('apollo-datasource-mongodb');
 class Artist extends MongoDataSource {
 	async getArtist(artistID) {
 		const artist = await this.findOneById(artistID);
-		artist.id = artist._id;
+		this.convertID(artist);
 		return artist;
 	}
 
 	async getArtists(fields = {}) {
 		const artists = await this.findByFields(fields);
-		artists.map((artist) => (artist.id = artist._id));
+		artists.map((artist) => this.convertID(artist));
 		return artists;
+	}
+
+	async getArtistsByIDs(ids) {
+		const artists = await this.findManyByIds(ids);
+		artists.map((artist) => this.convertID(artist));
+		return artists;
+	}
+
+	convertID(artwork) {
+		artwork.id = artwork._id;
 	}
 }
 
