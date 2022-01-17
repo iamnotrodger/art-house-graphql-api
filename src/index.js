caef54700148fb195cf21b5536ce318f06c87956
iamnotrodger <rodger.retanal24@gmail.com>
2022-01-17 12:41:28 -0500

    [update] Added resovler + datasource for artist to server

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ const dotenv = require('dotenv');
 const { ApolloServer } = require('apollo-server');
 const { MongoClient } = require('mongodb');
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const Artist = require('./datasources/artist');
 
 dotenv.config();
 
 const client = new MongoClient(process.env.MONGO_URI);
 client.connect();
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	dataSources: () => ({
+		artistAPI: new Artist(client.db().collection('artists')),
+	}),
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
 	console.log(`🚀  Server ready at ${url}`);
